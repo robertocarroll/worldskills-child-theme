919d6b1528207aa44c7f610a8db067d09210a4c0
robertocarroll <rob@wearethoughtfox.com>
2023-02-22 14:13:40 +0000

    Remove category from url

--- functions.php
+++ functions.php
@@ -228,3 +228,17 @@ function ws_breadcrumb() {
         echo '  </ol></nav>';
     }
 }       
+
+ // Remove /category/ from url 
+
+function remove_category( $string, $type )
+{ 
+        if ( $type != 'single' && $type == 'category' && ( strpos( $string, 'category' ) !== false ) )
+        {
+            $url_without_category = str_replace( "/category/", "/", $string );
+            return trailingslashit( $url_without_category );
+        }
+    return $string;
+}
+ 
+add_filter( 'user_trailingslashit', 'remove_category', 100, 2);
